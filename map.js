e45483b4ae01e52ea20fa47777f4fd2fa64b2b9f
Kyle Gilligan <kylegllgn@gmail.com>
2025-10-30 06:38:24 -0500

    fetcj update

--- map.js
+++ map.js
@@ -204,7 +204,7 @@ async function initMap() {
   }).addTo(map);
 
   try {
-  const results = await fetch("https://nonlaying-unjilted-johnathan.ngrok-free.app/api/v1/results", {
+  const results = await fetch("https://nonlaying-unjilted-johnathan.ngrok-free.dev/api/v1/results", {
     headers: { 
       'ngrok-skip-browser-warning': 'true'
     }
